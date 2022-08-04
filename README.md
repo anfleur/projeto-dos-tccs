5dd967b45411d5631037ba42524b57e78be108bc
anfleur <ana.zgermano2005@gmail.com>
2022-08-04 19:20:05 -0300

    primeiro commit no git!

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # projeto-dos-tccs
 Projeto do TCC de Ana Beatriz, Anna Laura, Luana e Kauã, sobre o armazenamento online dos tcc's guardados nas prateleiras biblioteca.
+
+# <a href="carrinho.php?acao=add&id=<?php echo $ln['id']; ?>" class="btn btn-outline-dark" > Adicionar</a> 
+
+# <img src="<?php echo 'img/'.$ln['imagem']; ?>" class="card-img-top" alt="..." style="width:100%;">
---
+++ src/conexao/conn.php
@@ -0,0 +1,15 @@
+<?php  
+
+    $hostname = "localhost";
+    $dbname = "sistcc";
+    $username = "root";
+    $password = "";
+
+
+    try {
+        $pdo = new PDO('mysql:host='.$hostname.';dbname='.$dbname, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // echo 'Conexão realizada com sucesso!';
+    } catch (PDOException $e) {
+        echo 'Error: '.$e->getMessage();
+    }
